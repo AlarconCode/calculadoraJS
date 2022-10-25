773933f2aa0aad23988ce6444a55a8656c69a18f
AlarconCode <julian.alarcon81@gmail.com>
2022-10-25 19:43:05 +0200

    actualizando rama2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // index.js (fichero de librería)
 // Funciones de la calculadora
 
-const suma = (op1,op2) => {return op1+op2}
+const suma = (op1,op2,op3) => {return op1+op2+op3}
 const resta = (op1,op2) => {return op1-op2}
 const producto = (op1,op2) => {return op1*op2}
 const division = (op1,op2) => {return op1/op2}
