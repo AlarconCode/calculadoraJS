8cfd569eee08041a1a3fe9ea851a17f3327758f0
AlarconCode <julian.alarcon81@gmail.com>
2022-10-25 20:01:11 +0200

    funcion suma modificada para recibir cuatro parametros

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 // index.js (fichero de librería)
 // Funciones de la calculadora
 
-<<<<<<< HEAD
-const suma = (op1,op2) => {return op1+op2}
-=======
 const suma = (op1,op2,op3,op4) => {return (op1+op2+op3+op4)}
->>>>>>> rama2
 const resta = (op1,op2) => {return op1-op2}
 const producto = (op1,op2) => {return op1*op2}
 const division = (op1,op2) => {return op1/op2}
