55fb03898526d590293c8b9d09d59609b1499258
AlarconCode <julian.alarcon81@gmail.com>
2022-10-26 18:12:54 +0200

    modifico la funcion suma para que reciba 3 parametros

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // index.js (fichero de librería)
 // Funciones de la calculadora
 
-const suma = (op1,op2,op3,op4) => {return (op1+op2+op3+op4)}
+const suma = (op1,op2,op3,op4) => {return (op1+op2+op3)}
 const resta = (op1,op2) => {return op1-op2}
 const producto = (op1,op2) => {return op1*op2}
 const division = (op1,op2) => {return op1/op2}
 const cuadrado = (op1,op2) => {op1*op2}
 
-module.exports = {suma, resta, producto, division}
+module.exports = {suma, resta, producto, division, cuadrado}
