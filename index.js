5082b70ed57df1c99b22e9595c315791ade895e8
AlarconCode <julian.alarcon81@gmail.com>
2022-10-27 14:33:28 +0200

    borrada funcion suma

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const producto = (op1,op2) => {return op1*op2}
 const division = (op1,op2) => {return op1/op2}
 const cuadrado = (op1,op2) => {op1*op2}
 
-module.exports = {suma, resta, producto, division}
+module.exports = {resta, producto, division}
